46feddda7fe93ee66a11191a1644cddd051ad94c
aswathcm2904 <aswath.cm2022cse@sece.ac.in>
2023-12-16 20:43:08 +0530

    Added matrix and list basics

---
+++ R Programming/Matrix and List.R
@@ -0,0 +1,12 @@
+mtr = matrix(c(5:29),5,5)
+mtr
+
+arr = array(c(1:9),dim = c(3,3,4,3))
+arr #will create the vector in 3 * 3 dim for 4 * 3 times
+
+
+vtr7=c(5,4.456,7,2,4,5)
+vtr8=c("Hey","Aswath")
+
+mylist=list(vtr7,vtr8)
+mylist
