a1a7a4776c0e0f59cd1b081c90febcc3a49b27be
aswathcm2904 <aswathcm29@gmail.com>
2023-12-16 21:26:47 +0530

    added operators

---
+++ R Programming/Operators.R
@@ -0,0 +1,25 @@
+# Arithmetic
+a = 6
+b = 4
+
+# Quotient
+print(paste("Quotient:", a/b))
+
+# Remainder
+print(paste("Remainder:", a%%b))
+
+# Exponential
+print(paste("Exponential:", a^b))
+
+# Floor division
+print(paste("Floor Division:", a%/%b))
+
+# Logical
+a = 6
+b = 4
+
+# Equality
+print(paste("a == b:", a == b))
+
+# Inequality
+print(paste("a != b:", a != b))
