0289cf6ce9abb9b04c2f391d6611247cdf244b10
aswathcm2904 <aswathcm29@gmail.com>
2023-12-17 11:51:17 +0530

    Added strings

---
+++ R Programming/Strings.R
@@ -0,0 +1,12 @@
+str1 <- "Hello world"
+str2 <- "This is a new data scientist who's going to change the future"
+
+print(paste(str1,str2))
+
+print(nchar(str1)) # number of characters in a string
+
+print(toupper(str1)) 
+
+print(tolower(str1))
+
+print(substr(str2,1,28))
