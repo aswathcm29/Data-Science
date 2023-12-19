f43b0f187e5c09edceb1f0d86bc8e945bc74f870
aswathcm2904 <aswathcm29@gmail.com>
2023-12-19 20:05:24 +0530

    Added box plot

--- R Programming/Introduction/ggplot2.R
+++ R Programming/Introduction/ggplot2.R
@@ -1,11 +1,18 @@
-library(ggplot2)
+library(ggplot2)  # Load the ggplot2 package
 
-data(iris)
+data(iris)  # Load the iris dataset that comes with R
 
+# Create a scatter plot using ggplot function
 ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
-  geom_point() +
-  labs(title = "Scatter Plot of Sepal Length vs Sepal Width",
-       x = "Sepal Length",
-       y = "Sepal Width")
+  geom_point() +  # Add points to the plot
+  labs(title = "Scatter Plot of Sepal Length vs Sepal Width",  # Set the plot title
+       x = "Sepal Length",  # Label the x-axis
+       y = "Sepal Width")  # Label the y-axis
 
-View(iris)
+
+# Box plot of Sepal.Length by Species
+ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
+  geom_boxplot() +
+  labs(title = "Box Plot of Sepal Length by Species",
+       x = "Species",
+       y = "Sepal Length")
